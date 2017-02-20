79466529d814bb29bb0c23e4f7c130ae786d905e
Dmitry Fast <df@proverstka.com>
2017-02-20 19:04:41 +0200

    Homework 3 done

--- home3/js/app.js
+++ home3/js/app.js
@@ -2,24 +2,74 @@
 	angular
 		.module('myApp', [])
 		.controller('MainController', ['$scope', function($scope) {
+			$scope.newTaskName = '';
 
 			$scope.ActivePage = angular.fromJson(window.localStorage.getItem('ActivePage')) || 0;
 
-			$scope.countPerPage = 5;
-
 			$scope.offset = $scope.countPerPage * $scope.ActivePage;
 
-			// angular.fromJson(window.localStorage.getItem('taskList')) || []
-			$scope.newTaskName = '';
 			$scope.taskList = angular.fromJson(window.localStorage.getItem('taskList')) || [];
 
-
-
-			$scope.pageCount = Math.ceil($scope.taskList.length / 5);
-
+			$scope.getPageCount = function() {
+				if($scope.countPerPage != null){
+					return $scope.pageCount = Math.ceil($scope.taskList.length / $scope.countPerPage);
+				} else {
+					return $scope.pageCount = 0;
+				}
+			};
 			$scope.getNumber = function(num) {
 				return new Array(num);
 			}
+
+            $scope.countPerPageEnum = [
+                {id: 0, value: null, 	text: 'все'},
+                {id: 1, value: 5,		text: 'пять'},
+                {id: 2, value: 10, 		text: 'десять'},
+                {id: 3, value: 20, 		text: 'двадцать'}
+            ];
+
+			$scope.countPerPage = $scope.countPerPageEnum[1].value;
+			$scope.getPageCount();
+
+
+
+
+
+			$scope.currentSort = null;
+			$scope.sortDirection = true;
+			$scope.changeCurrentSort = function(par){
+				if($scope.currentSort == par && $scope.sortDirection === false){
+					$scope.sortDirection = true;
+				} else {
+					$scope.currentSort = par;
+					$scope.sortDirection = false;
+				}
+				sortArrayBy(par);
+			}
+
+			function sortArrayBy(sortPar) {
+				// console.log('Сортировка по ' + sortPar + '. Перевернуть: ' + $scope.sortDirection);
+				$scope.taskList.sort(mySort);
+
+				function mySort(item2, item1){
+					if (item1[sortPar] < item2[sortPar]) {
+						if($scope.sortDirection){
+							return -1;
+						} else {
+							return 1;
+						}
+					}
+					if (item1[sortPar] > item2[sortPar]) {
+						if(!$scope.sortDirection){
+							return -1;
+						} else {
+							return 1;
+						}
+					}
+					return 0;
+				}
+			};
+
 
 
 			$scope.addTask = function() {
@@ -30,18 +80,17 @@
 						done: false
 					});
 					$scope.newTaskName = '';
-					$scope.pageCount = Math.ceil($scope.taskList.length / 5);
+					$scope.getPageCount();
 					$scope.changePage(0);
 				}
 			};
-
 
 			$scope.removeTask = function(taskId) {
 				for (var i = 0; i < $scope.taskList.length; i++) {
 					if ($scope.taskList[i].id === taskId) {
 						if (confirm('Are you sure you want to delete item `' + $scope.taskList[i].name + '`')) {
 							$scope.taskList.splice(i, 1);
-							$scope.pageCount = Math.ceil($scope.taskList.length / 5);
+							$scope.getPageCount();
 							if(document.querySelector('table').getElementsByTagName('tr').length < 4){
 								$scope.changePage($scope.ActivePage - 1);
 							}
@@ -50,7 +99,6 @@
 					}
 				}
 			};
-
 
 			$scope.removeAllTasks = function() {
 				if (confirm('Are you sure you want to delete all items?')) {
@@ -66,9 +114,33 @@
 					} else {
 						$scope.changePage(0);
 					}
-					$scope.pageCount = Math.ceil($scope.taskList.length / 5);
+					$scope.getPageCount();
 				}
 			};
+
+
+			$scope.changePage = function(newPage){
+				$scope.offset = $scope.countPerPage * newPage;
+				$scope.ActivePage = $scope.offset / $scope.countPerPage;
+				window.localStorage.setItem('ActivePage', angular.toJson($scope.ActivePage));
+				return false;
+			}
+			$scope.changePage($scope.ActivePage);
+
+
+			$scope.checkChecks = function(){
+				var allCheckedFn = true;
+				for(var i = $scope.ActivePage * $scope.countPerPage; i <  $scope.ActivePage * $scope.countPerPage + $scope.countPerPage; i++){
+					if($scope.taskList[i]){
+						if($scope.taskList[i].done == false){
+							allCheckedFn = false;
+							break;
+						}
+					}
+				}
+				$scope.allChecked = allCheckedFn;
+			};
+			$scope.checkChecks();
 
 			$scope.checkAllTasks = function() {
 				$scope.allChecked = !$scope.allChecked;
@@ -81,21 +153,24 @@
 				}
 			};
 
-			$scope.changePage = function(newPage){
-				$scope.allChecked = false;
-				$scope.offset = $scope.countPerPage * newPage;
-				$scope.ActivePage = $scope.offset / $scope.countPerPage;
-				window.localStorage.setItem('ActivePage', angular.toJson($scope.ActivePage));
-				return false;
-			}
-
-
-		$scope.$watch('taskList', function(newVal, oldVal) {
+			$scope.$watch('taskList', function(newVal, oldVal) {
 				if (newVal !== oldVal) {
 					window.localStorage.setItem('taskList', angular.toJson($scope.taskList));
+					$scope.checkChecks();
 				}
 			}, true);
+
+			$scope.$watch('countPerPage', function(newVal, oldVal) {
+				if (newVal !== oldVal) {
+					$scope.getPageCount();
+					$scope.checkChecks();
+					$scope.ActivePage = Math.ceil($scope.offset / $scope.countPerPage);
+				}
+			}, true);
+
+
 		}]);
+
 
 
 })();
